5492d6d68aeccc0ff93b343ce47bc62703ad9c2c
Swapna <swapna.sahu85@gmail.com>
2021-03-22 00:05:49 +0100

    changed background image

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,6 +1,6 @@
 import styled, { createGlobalStyle } from "styled-components";
 
-import BGImage from "./Images/Background.jpg";
+import BGImage from "./Images/image.jpg";
 export const GlobalStyle = createGlobalStyle`
 html {
     height: 100%
@@ -33,18 +33,11 @@ export const Wrapper = styled.div`
 
   h1 {
     font-family: Fascinate Inline;
-    background-image: linear-gradient(180deg, #fff, #87f1ff);
     font-weight: 400;
-    background-size: 100%;
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    -moz-background-clip: text;
-    -moz-text-fill-color: transparent;
-    filter: drop-shadow(2px 2px #0085a3);
     font-size: 70px;
     text-align: center;
     margin: 20px;
+    color:#5b4db0;
   }
   .start, .next {
     cursor: pointer;
